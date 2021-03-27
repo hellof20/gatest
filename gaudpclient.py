eba23060f209c764c3b2f2364f35a27c82c25793
Pan, Wen-Ming <smalljelly2018@gmail.com>
2021-03-27 16:18:01 +0800

    Create gaudpclient.py

---
+++ gaudpclient.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+import socket
+import time
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+for data in range(0,10):    
+    start = time.time()
+    s.sendto(str(data).encode('utf-8'), ('a459ec6218fbf2114.awsglobalaccelerator.com', 31337))
+    print(s.recv(1024).decode('utf-8'))
+    end = time.time()
+    running_time = (end-start) * 1000
+    print('time cost : %.2f ms' %running_time)
+s.close()
