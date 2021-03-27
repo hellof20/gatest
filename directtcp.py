a0dadfc3b1321755a663bdc9813177041bdc5bae
Pan, Wen-Ming <smalljelly2018@gmail.com>
2021-03-27 16:16:15 +0800

    Create directtcp.py

---
+++ directtcp.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+import socket
+import time
+
+connect_start = time.time()
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect(('54.64.75.187', 31337))
+connect_end = time.time()
+connect_time = (connect_end-connect_start) * 1000
+print('connect time cost : %.2f ms' %connect_time)
+
+for data in range(0,10):    
+    start = time.time()
+    s.send(str(data).encode('utf-8'))
+    print(s.recv(1024).decode('utf-8'))
+    end = time.time()
+    running_time = (end-start) * 1000
+    print('running time cost : %.2f ms' %running_time)
+s.close()
