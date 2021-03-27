3c479b9845572fe502b951aa706aaca20d26b077
Pan, Wen-Ming <smalljelly2018@gmail.com>
2021-03-27 16:21:33 +0800

    Update tcpserver.py

--- tcpserver.py
+++ tcpserver.py
@@ -1,19 +1,11 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
-
-# Author: David Manouchehri <manouchehri@protonmail.com>
-# This script will always echo back data on the UDP port of your choice.
-# Useful if you want nmap to report a UDP port as "open" instead of "open|filtered" on a standard scan.
-# Works with both Python 2 & 3.
-
 import socket
 import threading
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 server_address = '0.0.0.0'
 server_port = 31337
-
 server = (server_address, server_port)
 s.bind(server)
 s.listen(5)
