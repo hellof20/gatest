1ed172ca12c5cb34b5a417ac75d490792fc7c9ca
Pan, Wen-Ming <smalljelly2018@gmail.com>
2021-08-01 20:11:26 +0800

    Update directtcp.py

--- directtcp.py
+++ directtcp.py
@@ -4,13 +4,14 @@ import socket
 import time
 
 connect_start = time.time()
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.connect(('54.64.75.187', 31337))
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(('54.169.74.196', 30000))
 connect_end = time.time()
 connect_time = (connect_end-connect_start) * 1000
+print(s.recv(1024).decode('utf-8'))
 print('connect time cost : %.2f ms' %connect_time)
 
-for data in range(0,10):    
+for data in range(0,10):
     start = time.time()
     s.send(str(data).encode('utf-8'))
     print(s.recv(1024).decode('utf-8'))
