9f2a0138da8c9e18445d117f44817c0c3f53e297
Pan, Wen-Ming <smalljelly2018@gmail.com>
2021-08-03 11:30:19 +0800

    Update directtcp.py

--- directtcp.py
+++ directtcp.py
@@ -5,17 +5,23 @@ import time
 
 connect_start = time.time()
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('54.169.74.196', 30000))
+s.connect(('13.229.91.162', 30000))
 connect_end = time.time()
 connect_time = (connect_end-connect_start) * 1000
-print(s.recv(1024).decode('utf-8'))
 print('connect time cost : %.2f ms' %connect_time)
 
-for data in range(0,10):
-    start = time.time()
-    s.send(str(data).encode('utf-8'))
-    print(s.recv(1024).decode('utf-8'))
-    end = time.time()
-    running_time = (end-start) * 1000
-    print('running time cost : %.2f ms' %running_time)
+#first package
+s.send(str('a').encode('utf-8'))
+s.recv(1024).decode('utf-8')
+first = time.time()
+first_time = (first-connect_end)*1000
+print('first package time cost : %.2f ms' %first_time)
+
+#second package
+s.send(str('a').encode('utf-8'))
+s.recv(1024).decode('utf-8')
+second = time.time()
+second_time = (second - first) * 1000
+print('second package time cost : %.2f ms' %second_time)
+
 s.close()
