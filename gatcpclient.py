ff45529c325c141b6d049595395b34e4f8d7bb68
Pan, Wen-Ming <smalljelly2018@gmail.com>
2021-08-03 11:30:50 +0800

    Update gatcpclient.py

--- gatcpclient.py
+++ gatcpclient.py
@@ -4,18 +4,23 @@ import socket
 import time
 
 connect_start = time.time()
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.connect(('a459ec6218fbf2114.awsglobalaccelerator.com', 31337))
-#s.connect(('54.64.75.187', 31337))
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(('76.223.88.101', 30000))
 connect_end = time.time()
 connect_time = (connect_end-connect_start) * 1000
 print('connect time cost : %.2f ms' %connect_time)
 
-for data in range(0,10):    
-    start = time.time()
-    s.send(str(data).encode('utf-8'))
-    print(s.recv(1024).decode('utf-8'))
-    end = time.time()
-    running_time = (end-start) * 1000
-    print('running time cost : %.2f ms' %running_time)
+#first package
+s.send(str('a').encode('utf-8'))
+s.recv(1024).decode('utf-8')
+first = time.time()
+first_time = (first-connect_start)*1000
+print('first package time cost : %.2f ms' %first_time)
+
+#second package
+s.send(str('a').encode('utf-8'))
+s.recv(1024).decode('utf-8')
+second = time.time()
+second_time = (second - first) * 1000
+print('second package time cost : %.2f ms' %second_time)
 s.close()
